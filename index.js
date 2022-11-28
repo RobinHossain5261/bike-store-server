e0e1bcf74df40f793213026fa1d61559e809a465
Robin Hossain <robinhossain5261@gmail.com>
2022-11-28 19:21:29 +0600

    my orders query done

--- index.js
+++ index.js
@@ -34,6 +34,14 @@ async function run() {
             res.send(singleProducts)
         })
 
+        //my orders
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
             const result = await bookingsCollection.insertOne(booking);
